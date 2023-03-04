c433564d1f3d5e805505180abe1489aacab6ffad
Yohan Kim <yohan.kim@bagelcode.com>
2023-03-04 17:21:11 +0900

    write test code of JdbcShoppingmallRepositoryTest and save(), findById() of JdbcShoppingmallRepository

--- src/main/java/shoppingmall/shoppingmallspring/repository/JdbcShoppingmallRepository.java
+++ src/main/java/shoppingmall/shoppingmallspring/repository/JdbcShoppingmallRepository.java
@@ -1,9 +1,11 @@
 package shoppingmall.shoppingmallspring.repository;
 
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Repository;
 import shoppingmall.shoppingmallspring.domain.Cloth;
 
 import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,12 +20,63 @@ public class JdbcShoppingmallRepository implements ShoppingmallRepository{
 
     @Override
     public Cloth save(Cloth cloth) {
-        return null;
+        String sql = "insert into clothes(type, gender, size, color, image) values(?, ?, ?, ?, ?)";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, cloth.getType());
+            pstmt.setString(2, cloth.getGender());
+            pstmt.setString(3, cloth.getSize());
+            pstmt.setString(4, cloth.getColor());
+            pstmt.setString(5, cloth.getImage());
+            pstmt.executeUpdate();
+            rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                cloth.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return cloth;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
     }
 
     @Override
     public Optional<Cloth> findById(Long id) {
-        return Optional.empty();
+        String sql = "select * from clothes where id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            rs = pstmt.executeQuery();
+
+            Cloth result = new Cloth();
+            if (rs.next()) {
+                result.setId(rs.getLong("id"));
+                result.setType(rs.getString("type"));
+                result.setColor(rs.getString("color"));
+                result.setGender(rs.getString("gender"));
+                result.setSize(rs.getString("size"));
+                result.setImage(rs.getString("image"));
+            } else {
+                throw new SQLException("id로 옷 조회 실패");
+            }
+            return Optional.ofNullable(result);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
     }
 
     @Override
@@ -34,5 +87,37 @@ public class JdbcShoppingmallRepository implements ShoppingmallRepository{
     @Override
     public Optional<Cloth> deleteByTypeColor(String type, String color) {
         return Optional.empty();
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
+    {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
     }
 }
--- src/test/java/shoppingmall/shoppingmallspring/repository/JdbcShoppingmallRepositoryTest.java
+++ src/test/java/shoppingmall/shoppingmallspring/repository/JdbcShoppingmallRepositoryTest.java
@@ -4,8 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import shoppingmall.shoppingmallspring.domain.Cloth;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,21 +26,83 @@ class JdbcShoppingmallRepositoryTest {
         Cloth cloth = new Cloth();
         cloth.setType("tshirt");
         cloth.setColor("blue");
+        cloth.setGender("male");
+        cloth.setSize("small");
+        cloth.setImage("source/blue_t.png");
 
         repository.save(cloth);
 
-        assertThat(cloth).isEqualTo(repository.findById(cloth.getId()).get());
+        Cloth result = repository.findById(cloth.getId()).get();
+        assertThat(cloth.getType()).isEqualTo(result.getType());
+        assertThat(cloth.getColor()).isEqualTo(result.getColor());
+        assertThat(cloth.getGender()).isEqualTo(result.getGender());
+        assertThat(cloth.getSize()).isEqualTo(result.getSize());
+        assertThat(cloth.getImage()).isEqualTo(result.getImage());
     }
 
     @Test
     void findById() {
+        //given
+        Cloth cloth1 = new Cloth();
+        cloth1.setType("tshirt");
+        cloth1.setColor("blue");
+
+        Cloth cloth2 = new Cloth();
+        cloth2.setType("skirt");
+        cloth2.setColor("yellow");
+
+        //when
+        repository.save(cloth1);
+        repository.save(cloth2);
+
+        //then
+        assertThat(cloth1).isEqualTo(repository.findById(cloth1.getId()).get());
+        assertThat(cloth2).isEqualTo(repository.findById(cloth2.getId()).get());
     }
 
     @Test
     void findAll() {
+        //given
+        Cloth cloth1 = new Cloth();
+        cloth1.setType("tshirt");
+        cloth1.setColor("blue");
+
+        Cloth cloth2 = new Cloth();
+        cloth2.setType("skirt");
+        cloth2.setColor("yellow");
+
+        //when
+        repository.save(cloth1);
+        repository.save(cloth2);
+
+        //then
+        List<Cloth> list = repository.findAll();
+        assertThat(cloth1).isEqualTo(list.get(0));
+        assertThat(cloth2).isEqualTo(list.get(1));
     }
 
     @Test
     void deleteByTypeColor() {
+        //given
+        Cloth cloth1 = new Cloth();
+        cloth1.setType("tshirt");
+        cloth1.setColor("blue");
+
+        Cloth cloth2 = new Cloth();
+        cloth2.setType("skirt");
+        cloth2.setColor("yellow");
+
+        //when
+        repository.save(cloth1);
+        repository.save(cloth2);
+
+        //then
+        Long id_save = cloth1.getId();
+        Optional<Cloth> result1 = repository.deleteByTypeColor(cloth1.getType(), cloth1.getColor());
+        assertThat(result1.get()).isEqualTo(cloth1);
+        assertThat(repository.findById(id_save).get()).isEqualTo(null);
+
+        Optional<Cloth> result2 = repository.deleteByTypeColor(cloth1.getType(), cloth1.getColor());
+        assertThat(result2.get()).isEqualTo(null);
     }
 }
